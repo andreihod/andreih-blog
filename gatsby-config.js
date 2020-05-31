93599686252335141854dde2f7df8e729d43cbaa
Andrei <eu@andreih.com>
2020-05-31 18:35:18 -0300

    Add post and change external links

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Andrei's Blog`,
   },
   plugins: [
     {
@@ -24,11 +24,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/andreih1`,
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `Github`,
+            url: `https://github.com/andreihod`,
           },
         ],
       },
